1cbeb982af66457c663765ab4bd2736f3bee9a36
Igor Arnaut <48650366+igor722@users.noreply.github.com>
2024-03-21 09:58:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showSubnav(){
+    //console.log("mouse over!")
+    document.getElementById("sub-nav").style.display = "contents";
+}
+
+function hideSubnav(){
+    document.getElementById("sub-nav").style.display = "none";
+}
